a862fabebb8f5e329f16932fe86cb349198d4a64
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-03-28 22:11:31 -0300

    sd

--- resources/views/layouts/ecomerce_minimal/layout.blade.php
+++ resources/views/layouts/ecomerce_minimal/layout.blade.php
@@ -241,7 +241,7 @@
     </footer>
   </div>
 
-  <script src="{{url()}}{{ elixir('js/ecomerceApp.js')}} " ></script> 
+ 
 
   @if(Auth::guest())
              <script  src="https://unpkg.com/vue@2.5.17/dist/vue.min.js"></script> 
