7f009b6d5a55313314757d8545efb9a1fcf44941
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-04 18:57:51 -0300

    pl

--- resources/views/admin/marcas/partes/lista.blade.php
+++ resources/views/admin/marcas/partes/lista.blade.php
@@ -12,14 +12,13 @@
   
    <div class="admin-marca-contnedor-datos">
      
-     <div class="admin-marca-titulo"> {{$marca->name}}      </div>
-     
-     <div class="admin-user-lista-contenedor-acciones">
+     <div class="admin-marca-titulo">
         <a href="{{route('get_admin_marcas_editar', $marca->id)}}">
           <span class="boton-acciones-editar">
-           <i class="fas fa-edit"></i> 
+           {{$marca->name}}   <i class="fas fa-edit"></i>  
           </span>
-        </a>
+        </a> 
+
      </div>
    </div>
    
