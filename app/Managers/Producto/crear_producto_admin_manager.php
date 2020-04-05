e84a5f7a5d44e9cd97c7f8295d13e0478045c2dd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-04 21:26:46 -0300

    sd

--- app/Managers/Producto/crear_producto_admin_manager.php
+++ app/Managers/Producto/crear_producto_admin_manager.php
@@ -17,7 +17,7 @@ class crear_producto_admin_manager extends ManagerBase
       
       'name'              => 'required',
       'description'       => 'required',
-      'img'               => 'mimes:jpg',
+      'img'               => 'mimes:jpeg',
       'categoria_id'      => 'required',
       'marca_id'          => 'required',
       'precio'            => 'required',
