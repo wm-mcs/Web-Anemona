ba7dc8ad64b481ab285ecb523a9c10d964945cb2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-07 14:09:12 -0300

    df

--- resources/views/layouts/ecomerce_minimal/layout.blade.php
+++ resources/views/layouts/ecomerce_minimal/layout.blade.php
@@ -17,6 +17,7 @@
 
     <link rel="stylesheet" href="fonts/icomoon/style.css">
     <link rel="stylesheet" type="text/css" href="{{url()}}{{ elixir('css/ecomerce_minimal.css') }}">    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     
   </head>
   <body>
