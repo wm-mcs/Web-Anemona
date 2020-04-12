48cd832c5be1c8e50794ec050eea4d87fe2a23ce
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-12 19:32:36 -0300

    sd

--- resources/views/admin/productos/formularios_partes/datos_imagenes.blade.php
+++ resources/views/admin/productos/formularios_partes/datos_imagenes.blade.php
@@ -1,7 +1,7 @@
 
 <div class="formulario-label-fiel">
 <div class="formulario-label-aclaracion">
-	Las imágenes deben ser en formato <strong>jpg</strong> y en <strong>formato cuadrado</strong>.  Al menos de <strong>1000px x 1000px</strong> .
+	Las imágenes deben ser en formato <strong>jpg</strong> y en <strong>formato cuadrado</strong>.  Al menos de <strong>1000px x 1000px</strong>. Para que la web se vea linda la primera imagen de cada producto <strong>debe ser en fondo blanco</strong>.
 </div>
 {!! Form::label('img', 'Imagen', array('class' => 'control-label')) !!}
 {!! Form::file('img[]',['class'            => 'file',
