ac1b17e286174b79eccf4c0c4726bfd8f990b7ad
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-25 22:27:01 -0300

    sd

--- resources/views/paginas/vue/categorias_componente.blade.php
+++ resources/views/paginas/vue/categorias_componente.blade.php
@@ -82,9 +82,7 @@ template:'
       <span v-if="$root.mostrar_para_celuar" class="arrow-collapse collapsed" data-toggle="collapse" data-target="#collapseItem0"></span>
       <a href="shop.html" v-if="categorias.length">
 
-        <div class="cssload-container">
-<div class="cssload-speeding-wheel"></div>
-</div>
+
 
       Categorías
 
@@ -92,9 +90,9 @@ template:'
 
     
       <span v-else class="contiene-el-spiner">
-         <div class="cssload-container">
-               <div class="cssload-tube-tunnel"></div>
-         </div>
+                <div class="cssload-container">
+                  <div class="cssload-speeding-wheel"></div>
+                </div>
       </span>
 
      
