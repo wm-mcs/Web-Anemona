0cc0ebaecdf9345e3b992ba4c1b283af20de579a
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-04-25 22:00:27 -0300

    df

--- resources/views/paginas/vue/categorias_componente.blade.php
+++ resources/views/paginas/vue/categorias_componente.blade.php
@@ -82,7 +82,7 @@ template:'
       <span v-if="$root.mostrar_para_celuar" class="arrow-collapse collapsed" data-toggle="collapse" data-target="#collapseItem0"></span>
       <a href="shop.html" v-if="categorias.length">
 
- <div class="spinner-grow text-primary" role="status">
+        <div class="spinner-grow text-success" role="status">
   <span class="sr-only">Loading...</span>
 </div>
 
