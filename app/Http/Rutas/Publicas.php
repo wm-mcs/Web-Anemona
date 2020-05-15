e31ffea82d69ecb933454c922090993d9dc6bdc3
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-14 23:00:43 -0300

    fg

--- app/Http/Rutas/Publicas.php
+++ app/Http/Rutas/Publicas.php
@@ -35,7 +35,7 @@ Route::get('/' , [
     'as'   => 'getMarcas']
   );
 
-  Route::get('categoría/{categoria_name}-{categoria_id}-de-Be-Fitness-uruguay' , [                    
+  Route::get('Categoría/{categoria_name}-{categoria_id}-de-Be-Fitness-uruguay' , [                    
     'uses' => 'Publicas\Home_Public_Controller@get_pagina_de_categoria',
     'as'   => 'get_pagina_de_categoria']
   );
