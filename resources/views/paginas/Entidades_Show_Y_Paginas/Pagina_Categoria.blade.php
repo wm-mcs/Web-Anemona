ae83f084f7a05e9151023b87ac5ff5b0f461a040
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-14 22:33:57 -0300

    df

--- resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Categoria.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Categoria.blade.php
@@ -90,7 +90,7 @@
 
     
 
-   <div  class="Padding-de-secciones container products-wrap mt-5 mb-3">
+   <div  class="Padding-de-secciones container products-wrap pt-3 mt-5 mb-2">
    <h1 class="titulos-class text-uppercase text-color-black">{{ $Categoria->name_arreglado}}</h1>
    </div>
   <producto-show-interface :empresa="Empresa" titulo="Productos de la categoría {{ $Categoria->name_arreglado}}"></producto-show-interface>
