376dcbaedd67fdcb1b41a54c0060ba7b8959a67e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-19 15:25:57 -0300

    df

--- resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
@@ -40,16 +40,20 @@
 
 
 @section('imagen-grande-cabecera')
-  <div class="site-blocks-cover background_img
+  <div class="background_img
 background_img_fixed img_contacto  flex-row-center flex-justifice-space-around" data-aos="fade">
         <div class="container">
 
           <div class="row d-flex flex-column align-items-start p-4 mt-3 mt-lg-0 p-lg-0">
             <div class="col-12 col-lg-6 order-2 order-lg-1 ">
               <div class="">
-                <h1 class="mb-2 titulos-class text-uppercase text-white">Contactate ahora </h1>
-                <h2 class="sub-titulos-class text-white">
-                 Para pedir información de nuestro serviciós de mantenimiento para tu gimansio llenar el formulario de aquí abajo o usar cualquier otra vía de contacto están abajo.
+                <div class="col-10 mb-4">
+                  <img src="{{$Empresa->img_logo_cuadrado}}" class="img-fluid">
+                </div>
+                
+                <h1 class="mb-3 titulos-class text-uppercase text-white">Contactate ahora </h1>
+                <h2 class="sub-titulos-class text-white mb-3">
+                 Para pedir información de nuestro serviciós de mantenimiento para tu gimansio llenar el formulario de aquí abajo o usar cualquier otra vía de contacto que están abajo.
                 </h2>
                 
                
