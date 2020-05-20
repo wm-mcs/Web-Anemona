a7275277110afdb35fc4241fc68af7e1f0f2a358
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-05-20 14:07:19 -0300

    fg

--- resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
@@ -93,7 +93,7 @@ background_img_fixed img_contacto  flex-row-center flex-justifice-space-around" 
 
                 <div class="col-12 mb-5 mt-5 p-2 p-lg-5 titulos-class text-center text-color-black">Contatanos ahora a través ...</div>
                 <div class="col-6 col-lg-4 p-2 p-lg-4">
-                  <div class="titulos-class text-center text-color-primary sub-titulos-class mb-4">
+                  <div class="titulos-class text-center text-color-primary  mb-4">
                     <i class="fas fa-map-marker-alt"></i>
                   </div>
                   
@@ -102,7 +102,7 @@ background_img_fixed img_contacto  flex-row-center flex-justifice-space-around" 
                    </p>
                 </div>
                 <div class="col-6 col-lg-4 p-2 p-lg-4">
-                  <div class="titulos-class text-center text-color-primary sub-titulos-class mb-4">
+                  <div class="titulos-class text-center text-color-primary  mb-4">
                     <i class="fas fa-phone"></i>
                   </div>
                   <p class="parrafo-class color-text-gris text-center mb-2"> 
@@ -113,7 +113,7 @@ background_img_fixed img_contacto  flex-row-center flex-justifice-space-around" 
                   </p>
                 </div>
                 <div class="col-6 col-lg-4 p-2 p-lg-4">
-                  <div class=" titulos-class text-center text-color-primary sub-titulos-class mb-4">
+                  <div class=" titulos-class text-center text-color-primary mb-4">
                     <i class="fas fa-envelope"></i>
                   </div>
                   
