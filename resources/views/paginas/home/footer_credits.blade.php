a168c639e653de2463de494a594148a48446384d
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-01 12:50:32 -0300

    df

--- resources/views/paginas/home/footer_credits.blade.php
+++ resources/views/paginas/home/footer_credits.blade.php
@@ -1,5 +1,5 @@
 <div class="p-5 background-gris-4">
   <p class="m-0 text-center color-text-gris parrafo-class"> 
-   <i class="fas fa-code"></i> <span class="color-text-gris">Desarrollado por</span>   <a class="olor-text-white" href="https://mauricio.mwebs.com.uy/"> Mauricio Costanzo</a>
+   <i class="fas fa-code"></i> <span class="color-text-gris">Desarrollado por</span>   <a class="color-text-white" href="https://mauricio.mwebs.com.uy/"> Mauricio Costanzo</a>
   </p>
 </div>
