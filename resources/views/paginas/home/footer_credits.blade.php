ec3357064938d2015812c3e855df7fc3ddea0f2e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-01 12:42:45 -0300

    gh

--- resources/views/paginas/home/footer_credits.blade.php
+++ resources/views/paginas/home/footer_credits.blade.php
@@ -1,5 +1,5 @@
-<div class="p-4 background-gris-4">
+<div class="p-5 background-gris-4">
   <p class="m-0 text-center color-text-white parrafo-class"> 
-   <i class="fas fa-code"></i> Desarrollado por  <a href="https://mauricio.mwebs.com.uy/"> Mauricio Costanzo</a>
+   <i class="fas fa-code"></i> <span class="color-text-gris">Desarrollado por</span>   <a href="https://mauricio.mwebs.com.uy/"> Mauricio Costanzo</a>
   </p>
 </div>
