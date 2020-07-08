96ffa5b5fe196f0da9643ebe7b4550e9f0a09690
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-08 14:33:00 -0300

    df

--- resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
@@ -40,47 +40,34 @@
 
 
 @section('imagen-grande-cabecera')
-  <div class="background_img
+<div class="background_img
 background_img_fixed img_contacto " data-aos="fade">
-<div class="background-layer-layoute-opasity  d-flex flex-row justify-content-center align-items-center">
-        <div class="container">
-
-          <div class="row d-flex flex-column align-items-start p-4 mt-3 mt-lg-0 p-lg-0">
-            <div class="col-12 col-lg-6 order-2 order-lg-1 ">
-              <div class="">
-               
-                
-                <h1 class="mb-5 titulos-class text-uppercase text-white">Contáctate ahora </h1>
-                 <div class="row col-6 mb-5">
-                  <a href="{{url()}}">
-                    <img src="{{$Empresa->img_logo_cuadrado}}" class="img-fluid">
-                  </a> 
-                </div>
-                <p class="parrafo-class text-white text-bold mb-5">
-                 Para coordinar una visita a tu gimnasio o pedir más información, contáctate  usando cualquier vía de contacto que están aquí abajo. También puedes rellenar el formulario.
-                </p>
-                
-               
-                <div class="row">
-                    <div class="col-lg-12 p-1">
-                      <a href="#formulario_contacto" class="Boton-Primario-Relleno Boton-Fuente-Chica">
-                        Coordiná una visita a tu gimnasio ahora mismo  <i class="fas fa-angle-double-right"></i>
-                      </a> 
-                    </div>
-                                      
-                 </div> 
-                
-                                
-              </div> 
-              
-            </div>
-            
-             
-            
-            
+  <div class="background-layer-layoute-opasity  d-flex flex-row justify-content-center align-items-center">
+      <div class="container">
+        <div class="row d-flex flex-column align-items-start p-4 mt-3 mt-lg-0 p-lg-0">
+          <div class="col-12 col-lg-6 order-2 order-lg-1 ">
+            <div class="">
+              <h1 class="mb-5 titulos-class text-uppercase text-white">Contáctate ahora </h1>
+               <div class="row col-6 mb-5">
+                <a href="{{url()}}">
+                  <img src="{{$Empresa->img_logo_cuadrado}}" class="img-fluid">
+                </a> 
+              </div>
+              <p class="parrafo-class text-white text-bold mb-5">
+               Para coordinar una visita a tu gimnasio o pedir más información, contáctate  usando cualquier vía de contacto que están aquí abajo. También puedes rellenar el formulario.
+              </p>   
+              <div class="row">
+                  <div class="col-lg-12 p-1">
+                    <a href="#formulario_contacto" class="Boton-Primario-Relleno Boton-Fuente-Chica">
+                      Coordiná una visita a tu gimnasio ahora mismo  <i class="fas fa-angle-double-right"></i>
+                    </a> 
+                  </div>          
+               </div>       
+            </div> 
           </div>
         </div>
-</div>        
+      </div>
+  </div>        
 </div>
 @stop
 
@@ -89,7 +76,6 @@ background_img_fixed img_contacto " data-aos="fade">
 @section('contenido')
 
            <div class="container">
-             
             <div class="row mb-5" >
 
                 <div class="col-12 mb-5 mt-5 p-2 p-lg-5 titulos-class text-center text-color-black">Contatanos ahora a través ...</div>
@@ -132,44 +118,15 @@ background_img_fixed img_contacto " data-aos="fade">
 
      <div  class="site-section" id="formulario">
       <div class="container">
-
-
-
-
         <div class="row align-items-center justify-content-center">
-          
-          <div class="col-lg-6 order-2 order-lg-2">
-
-                
-              
-                
-
+          <div class="col-lg-6 order-2 order-lg-2">   
                <contacto-component :empresa="Empresa" :color="variables.input_color_primary" inline-template>
-               
                  @include('paginas.vue.Contacto.Contacto_comun')
-               
                </contacto-component>
-
-
-            
-            
-            
           </div>
-          
-          
         </div>
       </div>
     </div>
-    
-
-
-   
-
-    
-         
-       
-   
-     
 @stop 
 
 
