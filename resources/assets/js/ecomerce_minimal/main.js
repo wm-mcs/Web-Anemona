76ca4dfa43cd699d76ca94c4936e216f7503eadd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-10 21:14:10 -0300

    df

--- resources/assets/js/ecomerce_minimal/main.js
+++ resources/assets/js/ecomerce_minimal/main.js
@@ -4,34 +4,7 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
-	var slider = function() {
-		$('.nonloop-block-3').owlCarousel({
-	    center: false,
-	    items: 1,
-	    loop: true,
-	    smartSpeed: 700,
-			stagePadding: 15,
-	    margin: 20,
-	    autoplay: true,
-	    nav: true,
-			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
-	    responsive:{
-        600:{
-        	margin: 20,
-          items: 2
-        },
-        1000:{
-        	margin: 20,
-          items: 3
-        },
-        1200:{
-        	margin: 20,
-          items: 3
-        }
-	    }
-		});
-	};
-	slider();
+	
 
 
 	var siteMenuClone = function() {
