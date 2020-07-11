1dab3b0c5a4cfe0e08363c67825dc233bcce567b
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-10 21:29:10 -0300

    df

--- resources/views/paginas/home/BeFitnessMarcaDestacada.blade.php
+++ resources/views/paginas/home/BeFitnessMarcaDestacada.blade.php
@@ -1,6 +1,6 @@
 <section class="Padding-de-secciones background-gris-1">
 	<div class="container">
-		<div class="row aligne-items-center justify-content-center">
+		<div class="row align-items-center justify-content-center">
 			<div class="col-12 col-lg-3 p-lg-3">
 				<img data-src="{{url()}}/imagenes/Marcas/Sportsart/logo.png" class="img-fluid">
 			</div>
@@ -9,7 +9,11 @@
 			</div>	
 			<div class="col-12 col-lg-5 p-2 p-lg-3">
 				<h3 class="mb-2 color-text-gris sub-titulos-class"> <b>SportsArt - ECO-NATURAL</b> </h3>
-				<p class="mb-2 color-text-gris">Junto a un diseño completamente renovado, SportsArt decide lanzar dos productos completamente renovados al mercado. ¿te imaginás combinar movimientos de elíptica, bicicleta y escalador, en un solo equipo?</p>
+				<p class="mb-2 color-text-gris">
+	                <small>
+					Junto a un diseño completamente renovado, SportsArt decide lanzar dos productos completamente renovados al mercado. ¿te imaginás combinar movimientos de elíptica, bicicleta y escalador, en un solo equipo?
+				    </small>
+			    </p>
 				<a href="" class="Boton-Fuente-Muy-Chica Boton-Primario-Sin-Relleno">Conocé la nueva linea de equipos cardio de SportsArt <i class="fas fa-angle-double-right"></i></a>
 
 				
