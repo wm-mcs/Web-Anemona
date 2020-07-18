e2807b1ecf6d2fab3f465db3d699ef8270753ee0
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-18 13:55:21 -0300

    df

--- resources/views/layouts/admin_layout/columna_derecha/columna.blade.php
+++ resources/views/layouts/admin_layout/columna_derecha/columna.blade.php
@@ -7,8 +7,7 @@
 
  <div class="w-100 p-4">
    @if(Auth::user()->role === 'adminMcos522')
-   <div class="row p-2 mb-5 text-left">
-        
+   <div class="row  mb-5 text-left">        
         <a class="d-block col-12  mb-1" href="{{route('get_admin_users')}}">
           <p class="m-0"> Usuarios</p>
         </a>
@@ -23,32 +22,25 @@
         
     </div>
    @endif
+   <div class="row  mb-5 text-left">        
+        <a class="d-block col-12  mb-1" href="{{route('get_datos_corporativos')}}">
+          <p class="m-0">  La Empresa</p>
+        </a>
 
-   <div id="admin-col-admin">
-        <a href="{{route('get_datos_corporativos')}}">
-            <li class="admin-columna-li mi-float-right"><i class="fas fa-building"></i> La Empresa</li>
+        <a class="d-block col-12  mb-1" href="{{route('get_admin_categorias')}}">
+          <p class="m-0"> Categorías</p>
+        </a> 
+
+        <a class="d-block col-12  mb-1" href="{{route('get_admin_productos')}}">
+          <p class="m-0"> Productos</p>
         </a>   
-        <a href="{{route('get_admin_categorias')}}">
-          <li class="admin-columna-li mi-float-right"><i class="fas fa-bars"></i> Categorias</li>
-        </a>  
-         <a href="{{route('get_admin_productos')}}">
-          <li class="admin-columna-li mi-float-right"><i class="fab fa-product-hunt"></i> Productos</li>
-        </a>     
         
     </div>
 
-</div class="w-90">
 
-    <div >
-        <a href="{{route('get_home')}}">
-            <p><small>Nombre</small></p>
-        </a>
-    </div>
-     <div >
-        <a href="{{route('logout')}}">
-            <p><small>Salir</small></p>
-        </a>
-    </div>
+</div>
+
+  
 
     
 </div>
