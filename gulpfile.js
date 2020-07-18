e642c51f21600d43f658c2c6f636491523747c14
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-18 13:02:59 -0300

    df

--- gulpfile.js
+++ gulpfile.js
@@ -43,10 +43,9 @@ elixir(function(mix) {
         
         'Template_creative/jquery.js',
         'Template_creative/bootstrap.bundle.js',
-        'Customs/helper_generales.js',
-        'Plugins/Plug-bootstrap-fileinput v4.3.7.js',
-        'Customs/mis-file_input.js',
-        'Customs/admin_eventos.js'
+        'Customs/helper_generales.js',        
+        'Customs/admin_eventos.js',
+        'Plugins/Plug-lazyLoadXT.js',
        
 
        ],'public/js/admin.js');
