7fd9f77d0c6e4714848cce6e777731326d41fb43
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-18 14:45:13 -0300

    df

--- resources/assets/js/Customs/admin_eventos.js
+++ resources/assets/js/Customs/admin_eventos.js
@@ -1,14 +1,5 @@
-  // buscador de eventos por marca
-  $('body').on('change','#select_marcas_en_evento',function(e)
-  {
-   e.preventDefault();   
 
-   var form  = $(this).parents() ;  
-   form.submit();
-  });
-
-
-  $('body').on('click','.admin-boton-editar',function(e)
+  $('body').on('click','.admin-boton-editar , .disparar-este-form',function(e)
   {
    e.preventDefault();   
 
@@ -18,26 +9,3 @@
 
 
 
-  // buscador de eventos por marca
-  $('body').on('click','.editar-evento-guardar',function(e)
-  {
-   e.preventDefault();   
-
-   var form  = $(this).parents();  
-
-   $("input[name=tipo_de_boton]").val('guardar');
-
-      form.submit();
-  });
-
-  // buscador de eventos por marca
-  $('body').on('click','.editar-evento-guardar-y-salir',function(e)
-  {
-   e.preventDefault();   
-
-   var form  = $(this).parents();  
-
-   $("input[name=tipo_de_boton]").val('guardar-y-salir');
-
-      form.submit();
-  });
