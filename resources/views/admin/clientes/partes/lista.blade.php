c6be529f60a7539bcc271e070872a527442cd923
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-18 14:28:50 -0300

    sd

--- resources/views/admin/clientes/partes/lista.blade.php
+++ resources/views/admin/clientes/partes/lista.blade.php
@@ -2,18 +2,12 @@
 {{-- E s t a   e s   l a   v i s t a   p a r a   a d m i n  --}}
 @if(isset($Mostrar_admin) && $Mostrar_admin == true)
 <div class="col-md-6 col-lg-4 mb-4">
-    <div class="servicio_lista service">      
-      <a href="{{$Route}}">
-        <img data-src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="servicio_lista_imagen">
+    <div class="servicio_lista service">
+      <a href="{{$Route}}" class="d-flex flex-column align-items-center">
+        <img data-src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="img-cover-con-formato-cuadrado rounded-circle">
       </a>              
       <div class="p-3 mt-2">
-        <p class="mb-3">
-          @if($Entidad->estado == 'si')
-            <span class="color-text-success">Activo</span>
-          @else
-            <span class="color-text-gris">Inactivo</span>
-          @endif
-        </p>   
+        @include('admin.partials.atributo_estado_lista')
         <h3 class="sub-titulos-class   mb-2">
           <a href="{{$Route}}" class="font-primary text-color-secondary">
            {{$Entidad->name}}
@@ -22,37 +16,31 @@
         <p class="color-text-gris mb-2 ">
          {{$Entidad->descripcion_breve}}
         </p>
-        <p class="">
+        <p>
           <a href="{{$Route}}"> Editar  <i class="fas fa-chevron-right"></i></a>
-        </p>  
-                    
+        </p>                
       </div>
     </div>
 </div>
 @else 
 {{--  E s t a   e s   p a r a   e l   p ú b l i c o  --}}
-<div class="col-12 col-lg-10 mb-4 ">
-    <div class="servicio_lista service position-relative">  
-        <a href="{{$Route}}"> 
-         <img  data-src="{{$Entidad->url_img_foto_principal}}" alt="{{$Entidad->descripcion_breve}}" class="tour-img-listado">
-        </a> 
-        <a href="{{$Route}}">         
-          <div class="tour-contiene-datos-listado">
-            <div class="sub-titulos-class text-bold color-text-white mb-2">
-             {{$Entidad->name}}
-            </div> 
-            
-             <div class="parrafo-class color-text-white mb-1"> 
-              Próxima fecha <i class="far fa-clock"></i> {{$Entidad->fecha->format('d-m-Y')}}.
-             </div>
-             <div class="parrafo-class color-text-white mb-2"> 
-              Tour de <span class="text-bold">{{$Entidad->cantidad_de_dias}}</span> días.
-             </div>
-            <p>
-            <a href="{{$Route}}"> Explorar el contenido del tour  <i class="fas fa-chevron-right"></i></a>
-           </p>
-          </div>
-        </a>
+<div class="col-md-6 col-lg-4 mb-4">
+    <div class="servicio_lista service">
+      <div class="d-flex flex-column align-items-center">
+        <img data-src="{{$Entidad->url_img_foto_principal_chica}}" alt="{{$Entidad->descripcion_breve}}" class="img-cover-con-formato-cuadrado rounded-circle">
+      </div>              
+      <div class="p-3 mt-2">
+        @include('admin.partials.atributo_estado_lista')
+        <h3 class="sub-titulos-class   mb-2">
+          <p  class="font-primary color-text-gris">
+           {{$Entidad->name}}
+          </p>                
+        </h3>
+        <p class="color-text-gris mb-0 ">
+         {{$Entidad->descripcion_breve}}
+        </p>
+                        
+      </div>
     </div>
 </div>
 @endif
