28ba5a762e3aca62f3252c357b1e27f94cd854b4
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-24 19:11:59 -0300

    fg

--- resources/views/paginas/vue/marca-lista.blade.php
+++ resources/views/paginas/vue/marca-lista.blade.php
@@ -24,7 +24,7 @@ template:'
 
   <div class="col-6 col-lg-3 mb-4">
   	<a :href="marca.route" class="item">
-	    <div class="w-100 Cliente-contenedor">
+	    <div class="w-100 ">
 	      <div class="d-flex flex-column align-items-center">
 	        <img :src="marca.url_img_foto_principal" :alt="marca.name" class="img-cover-con-formato-rectangular border-gris">
 	      </div>   
