0a19e825c1b87eb096296b08868b585026d06cfd
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-24 13:18:21 -0300

    hj

--- app/Http/Rutas/Marcas/Rutas_Marcas.php
+++ app/Http/Rutas/Marcas/Rutas_Marcas.php
@@ -2,30 +2,30 @@
 
 Route::get('get_admin_marcas',
 [
-  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@get_admin_marcas',
+  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@get_admin',
   'as'    => 'get_admin_marcas'
 ]);
 
 Route::get('get_admin_marcas_crear',
 [
-  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@get_admin_marcas_crear',
+  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@get_admin_crear',
   'as'    => 'get_admin_marcas_crear'
 ]);
 
 Route::post('set_admin_marcas_crear',
 [
-  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@set_admin_marcas_crear',
+  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@set_admin_crear',
   'as'    => 'set_admin_marcas_crear'
 ]);
 
 Route::get('get_admin_marcas_editar{id}',
 [
-  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@get_admin_marcas_editar',
+  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@get_admin_editar',
   'as'    => 'get_admin_marcas_editar'
 ]);
 
 Route::patch('set_admin_marcas_editar{id}',
 [
-  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@set_admin_marcas_editar',
+  'uses'  => 'Admin_Empresa\Admin_Marcas_Controllers@set_admin_editar',
   'as'    => 'set_admin_marcas_editar'
-]);
+]);  
