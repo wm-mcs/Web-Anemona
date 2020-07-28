bc1043ca6dac506ac46c825f92da4718af246c0f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-28 11:29:20 -0300

    fg

--- resources/views/admin/marcas/formularios_partes/datos_imagenes.blade.php
+++ resources/views/admin/marcas/formularios_partes/datos_imagenes.blade.php
@@ -18,39 +18,34 @@
 </p>
 
 	@if($Entidad->imagenes->count() > 0)
-    <div class="row  p-3 mt-4" >
+<div class="row  p-3 mt-4" >
 
-        @foreach($Entidad->imagenes as $Imagen)
-        <div class="col-6">
-          
-        
-         <div class="mt-3 position-relative img-border-grosor  @if($Imagen->es_imagen_principal) img-principal  @endif ">
-          <img class="img-fluid img-cover img-altura-chica" data-src="{{$Imagen->url_img}}">   
+    @foreach($Entidad->imagenes as $Imagen)
+    <div class="col-6">
+      
+    
+     <div class="mt-3 position-relative img-border-grosor  @if($Imagen->es_imagen_principal) img-principal  @endif ">
+      <img class="img-fluid img-cover img-altura-chica" data-src="{{$Imagen->url_img}}">   
 
-            @if($Imagen->es_imagen_principal)
-            <span class="img-element-bottom-left text-color-primary">
-              <i class="fas fa-star"></i>
-            </span>
-            @else
-            <a class="img-element-bottom-left" href="{{$Imagen->cambiar_a_pricnipal_route}}">
-              <i class="far fa-star"></i>
-            </a>
-            <a href="{{$Imagen->eliminar_route}}" class="img-element-bottom-right">
-            <i class="fas fa-trash"></i>
-            </a>
-            @endif
-         </div>
-        </div>
+        @if($Imagen->es_imagen_principal)
+        <span class="img-element-bottom-left text-color-primary">
+          <i class="fas fa-star"></i>
+        </span>
+        @else
+        <a class="img-element-bottom-left" href="{{$Imagen->cambiar_a_pricnipal_route}}">
+          <i class="far fa-star"></i>
+        </a>
+        <a href="{{$Imagen->eliminar_route}}" class="img-element-bottom-right">
+        <i class="fas fa-trash"></i>
+        </a>
+        @endif
+     </div>
+    </div>
     @endforeach
 
       
   
   </div>
-		@endforeach
-
-			
-	
-	</div>
 	@else
 	<p class="p-2 mt-4 mb-3 col-12 text-center color-text-gris parrafo-class">
 			No hay ninguna imagen.
