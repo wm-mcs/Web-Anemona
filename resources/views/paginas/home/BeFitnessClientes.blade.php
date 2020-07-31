a75b093cfc869078c82b225215dc55c3f4bd7df2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-31 15:49:15 -0300

    fg

--- resources/views/paginas/home/BeFitnessClientes.blade.php
+++ resources/views/paginas/home/BeFitnessClientes.blade.php
@@ -4,11 +4,15 @@
 			<div class="col-12 text-center mb-5 titulos-class text-color-black">
 				Confian en nosotros ...
 			</div>
+			<div class="col-12">
+				
+			
 			@foreach($Clientes as $Entidad )	
 				  {{--*/ $Mostrar_admin  = false /*--}}
 		          {{--*/ $Entidad        = $Entidad /*--}}
 		          @include('admin.clientes.partes.lista')
 			@endforeach
+			</div>
 		</div>
 	</div>
 </section>
