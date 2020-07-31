6081bda025ff8ad7224b0917bfdef44cf6fdbf9f
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-30 22:49:52 -0300

    sd

--- resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Contacto.blade.php
@@ -142,7 +142,7 @@ background_img_fixed img_contacto " >
 @stop
 
 @section('vue-componenetes-cdn')
-  <script  src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script> 
+  
   {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-select/2.6.2/vue-select.js"></script> --}}
-  <script  src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script>
+  {{-- <script  src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script> --}}
 @stop
