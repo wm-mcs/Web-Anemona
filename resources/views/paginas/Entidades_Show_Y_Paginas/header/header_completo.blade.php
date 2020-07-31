276b80fc7789afff57e5ad0dad41e023a9bf4a71
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-31 16:25:40 -0300

    df

--- resources/views/paginas/Entidades_Show_Y_Paginas/header/header_completo.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/header/header_completo.blade.php
@@ -20,10 +20,10 @@
                   
                     <li :class="getClassUlLI"><a :class="getClassItemsNav" href="{{route('get_home')}}" >Inicio</a></li>
 
-                     <menu-primer-triada  v-if="$root.categorias.length" name_padre="Categorías" url_padre="{{route('get_home')}}" >
+                     <menu-primer-triada  v-if="$root.Categorias.length" name_padre="Categorías" url_padre="{{route('get_home')}}" >
                       /* O p c i o n e s   d e l   m e n ú   */
                       <template slot="opciones" >
-                        <li v-for="categoria in $root.categorias" class="header-li-primer-tria">
+                        <li v-for="categoria in $root.Categorias" class="header-li-primer-tria">
                            <a :href="categoria.route">@{{categoria.name_arreglado}}</a>
                         </li>
                       </template> 
