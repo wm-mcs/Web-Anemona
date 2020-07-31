885a85c65b19c60c4b38443bb9818e15444ea5de
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-07-30 21:27:04 -0300

    df

--- app/Entidades/PortadaDePagina.php
+++ app/Entidades/PortadaDePagina.php
@@ -5,12 +5,14 @@ namespace App\Entidades;
 use Illuminate\Database\Eloquent\Model;
 use App\Entidades\Traits\entidadesMetodosComunes;
 use App\Entidades\Traits\entidadesTagsTitleMetodos;
+use App\Entidades\Traits\entidadImagen;
 
 class PortadaDePagina extends Model
 {
 
     use entidadesMetodosComunes;
     use entidadesTagsTitleMetodos;
+    use entidadImagen;
 
     protected $table            ='portada_de_paginas';    
     protected $fillable         = ['name', 'description'];
