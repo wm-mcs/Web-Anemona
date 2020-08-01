fd7b338ea89449df01971748d96205b5812f2f50
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-01 10:31:22 -0300

    fg

--- resources/views/paginas/vue/producto_lista_component.blade.php
+++ resources/views/paginas/vue/producto_lista_component.blade.php
@@ -32,7 +32,7 @@ computed:{
 
 template:'
 
-  <div v-if="formato_bloque" class="col-6 col-md-6 col-lg-3">
+  <div v-lazy-container="{ selector: 'img' }" v-if="formato_bloque" class="col-6 col-md-6 col-lg-3">
     <a :href="producto.route" class="item">
       <img :data-src="producto.url_img_foto_principal_chica" :alt="producto.name" class="img-fluid">
       <div class="item-info">
