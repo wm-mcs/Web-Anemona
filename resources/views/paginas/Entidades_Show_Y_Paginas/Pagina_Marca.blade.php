eee13fbdd33832baa32f8ee464400d24d140ccf6
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-03 11:31:19 -0300

    gh

--- resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Marca.blade.php
+++ resources/views/paginas/Entidades_Show_Y_Paginas/Pagina_Marca.blade.php
@@ -90,9 +90,6 @@
             <div class="col-lg-6 mb-2 mb-lg-0my-1 order-1 order-lg-2 d-flex flex-row align-items-center justify-content-start justify-content-lg-center">
                 <img class="img-fluid p-3 p-lg-4" data-src="{{ $Marca->url_img_foto_principal}}" alt="{{ $Marca->name_arreglado}} en Uruguay. Representación oficial por  {{ $Empresa->name}} " class="">
             </div>
-             
-            
-            
           </div>
         </div>
         
